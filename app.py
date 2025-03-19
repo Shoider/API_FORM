dde5b359ce0423daff9b77a3a79e1a6a737b78ac
BrandonHS <branditis1211@gmail.com>
2025-03-19 15:59:21 -0600

    fix(app): Removed libraries unused

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from flask import Flask, request, jsonify, send_file
-from flask_cors import CORS
-from io import BytesIO
-import pandas as pd
-from fpdf import FPDF
 from logger.logger import Logger
 from schemas.schema import RegistroSchema 
 from routes.route import FileGeneratorRoute  
-from marshmallow import ValidationError
-import os
 
 app = Flask(__name__)
 
