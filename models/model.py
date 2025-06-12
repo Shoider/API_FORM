19b8850ec0982e862ccd570d4249e63a0b12cbe9
BrandonHS <branditis1211@gmail.com>
2025-06-12 10:12:45 -0600

    fix(models): Modificacion de base de datos usada

--- models/model.py
+++ models/model.py
@@ -42,7 +42,7 @@ class BDModel:
                 authMechanism="SCRAM-SHA-256",
                 serverSelectionTimeoutMS=5000,
             )
-            self.db = self.client["formularios"]
+            self.db = self.client["project"]
 
             if self.db.list_collection_names():
                 self.logger.info("Connected to MongoDB successfully")
